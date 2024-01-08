3155bd3cd43fabfb3479fd3a21b3aa602a54635a
Romka <im.romkasmagin@gmail.com>
2024-01-08 10:50:02 +0300

    I connected the module from the first task to the main file and organized the call of optional functions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os_module_operation
+
+
 if __name__ == '__main__':
     print("В данной программе будет продемонстрировано "
           "выполнение домашнего задания под номером девять.\n"
@@ -8,3 +11,9 @@ if __name__ == '__main__':
           u"\n"
           "Или воспользуйтесь следующим https:\n"
           u"")
+    os_module_operation.print_os_name()
+    os_module_operation.print_path_to_current_directory()
+    os_module_operation.sort_file_by_extension()
+    os_module_operation.get_size_folder()
+    os_module_operation.rename_test_in_hello()
+    input('Первое задание отработало, для продолжение нажмите Enter...')
